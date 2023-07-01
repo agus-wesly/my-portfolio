55a0768fd8f24918746d671562ea55149ff0f918
aguswesly <agustchannel@gmail.com>
2023-07-01 17:04:37 +0700

    redirecting the site

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
-import { Navbar, Project, Heading, Skill, Talk, Footer } from "./components";
-import { useState, useEffect, useRef } from "react";
+import { Navbar, Project, Heading, Skill, Talk, Footer } from './components'
+import { useState, useEffect, useRef, useLayoutEffect } from 'react'
 
 function App() {
-  const [toggle, setToggle] = useState(false);
-  const headingRef = useRef(null);
-  const skillRef = useRef(null);
-  const projectRef = useRef(null);
-  const talkRef = useRef(null);
+  const [toggle, setToggle] = useState(false)
+  const headingRef = useRef(null)
+  const skillRef = useRef(null)
+  const projectRef = useRef(null)
+  const talkRef = useRef(null)
 
-  useEffect(() => {
-    if (toggle) {
-      document.body.classList.add("blurr");
-    } else {
-      document.body.classList.remove("blurr");
-    }
-  }, [toggle]);
+  useLayoutEffect(() => {
+    const anch = document.createElement('a')
+    anch.href = 'https://agus-wesly.vercel.app/'
+    anch.click()
+  }, [])
 
   return (
     <>
-      <div className={`font-Inter px-6 py-5 lg:px-44 lg:py-11 bg-primary max-w-full relative overflow-x-hidden h-full`}>
-        <Navbar toggle={toggle} setToggle={setToggle} headingRef={headingRef} skillRef={skillRef} projectRef={projectRef} talkRef={talkRef} />
+      <div
+        className={`font-Inter px-6 py-5 lg:px-44 lg:py-11 bg-primary max-w-full relative overflow-x-hidden h-full`}
+      >
+        <Navbar
+          toggle={toggle}
+          setToggle={setToggle}
+          headingRef={headingRef}
+          skillRef={skillRef}
+          projectRef={projectRef}
+          talkRef={talkRef}
+        />
         <Heading headingRef={headingRef} talkRef={talkRef} />
         <Skill skillRef={skillRef} />
         <Project projectRef={projectRef} />
@@ -27,7 +34,7 @@ function App() {
         <Footer />
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
